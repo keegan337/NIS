d85c57182a60bd2c980576eefd8ac77d353a2e33
Stuart Mesham <stumes3@gmail.com>
2020-05-19 17:15:57 +0200

    added documentation for root certificate generator

--- src/main/java/GenerateRootCert.java
+++ src/main/java/GenerateRootCert.java
@@ -21,6 +21,10 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
+/**
+ * Generates keys for the certificate authority.
+ * These keys will be used to sign certificates for each user.
+ */
 public class GenerateRootCert {
 	public static void main(String[] args) throws NoSuchAlgorithmException, OperatorCreationException, IOException, KeyStoreException, CertificateException {
 		System.out.println("generating key pair");
@@ -50,6 +54,7 @@ public class GenerateRootCert {
 				kp.getPublic()
 		);
 
+		//Sign the certificate using the private key (with the SHA1withRSA signing algorithm)
 		X509CertificateHolder certHolder = certGen.build(new JcaContentSignerBuilder("SHA1withRSA").build(kp.getPrivate()));
 
 		System.out.println("saving certificate to file");
@@ -63,16 +68,23 @@ public class GenerateRootCert {
 		System.out.println("saving private key to file");
 
 		//Write private key to file
+
+		/*
+		 * The private key is saved in a password-protected file called a keystore.
+		 * A keystore can contain many keys and each key is protected with its own password.
+		 * In this case we are only saving one key into this keystore: the certificate authority's private key.
+		 */
 		KeyStore store = KeyStore.getInstance("PKCS12");
 		store.load(null, null);
 
 		X509Certificate cert = new JcaX509CertificateConverter().getCertificate(certHolder);
 		Certificate[] chain = new Certificate[]{cert};
 
+		//Here we specify the password for the key
 		store.setKeyEntry("uct", kp.getPrivate(), "123".toCharArray(), chain);
 
 		fileOutputStream = new FileOutputStream("uct.p12");
-		store.store(fileOutputStream, "123".toCharArray());
+		store.store(fileOutputStream, "123".toCharArray()); //Here we specify the password for the keystore
 		fileOutputStream.flush();
 		fileOutputStream.close();
 	}
