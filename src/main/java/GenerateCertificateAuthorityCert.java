0f659668ea8ae2c585a169ca8267b02427d833a7
Stuart Mesham <stumes3@gmail.com>
2020-06-08 01:04:28 +0200

    updated CA cert generating code

--- src/main/java/GenerateCertificateAuthorityCert.java
+++ src/main/java/GenerateCertificateAuthorityCert.java
@@ -1,12 +1,15 @@
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.operator.OperatorCreationException;
 
 import java.io.IOException;
 import java.security.KeyPair;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * Generates keys for the certificate authority.
@@ -15,6 +18,7 @@ import java.security.cert.CertificateException;
 public class GenerateCertificateAuthorityCert {
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, OperatorCreationException, IOException, KeyStoreException, CertificateException {
+		System.out.println("generating key pair");
 		KeyPair kp = CertificateUtils.generateKeyPair();
 		
 		System.out.println("creating signed certificate");
@@ -25,6 +29,8 @@ public class GenerateCertificateAuthorityCert {
 		CertificateUtils.saveCertToFile(certHolder, "uct.der");
 
 		System.out.println("saving private key to file");
-		CertificateUtils.saverPkToFile(kp, certHolder, "uct", "123", "uct.p12", "123");
+		X509Certificate cert = new JcaX509CertificateConverter().getCertificate(certHolder);
+		Certificate[] certChain = new Certificate[]{cert};
+		CertificateUtils.saveToPKCS12(kp, certHolder, certChain, "uct", "123", "uct.p12", "123");
 	}
 }
